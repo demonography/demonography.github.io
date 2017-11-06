cc55f1b8a60ab908cf910e76791768bb26ea813c
ris <iris.wong@zgm.ca>
2017-11-05 17:01:45 -0700

    added MD file for glossary terms.

--- intro-to-web-design/glossary/gruntfile.js
+++ intro-to-web-design/glossary/gruntfile.js
@@ -1,33 +1,68 @@
 module.exports = function(grunt) {
-		grunt.initConfig({
-				pkg: grunt.file.readJSON('package.json'),
-				sass: {
-						dist: {
-								options: {
-										style: 'compressed'
-								},
-								files: {
-										'css/main.css': 'css/main.scss'
-								}
-						}
-				},
-				postcss: {
-						options: {
-								processors: require('autoprefixer')({browsers: 'last 2 versions'})
-						},
-						dist: {
-								src: 'css/*.css'
-						}
-				},
-				watch: {
-						sass: {
-								files: ['css/**/*.scss'],
-								tasks: ['sass', 'postcss']
-						}
-				}
-		});
-		grunt.loadNpmTasks('grunt-contrib-sass');
-		grunt.loadNpmTasks('grunt-postcss');
-		grunt.loadNpmTasks('grunt-contrib-watch');
-		grunt.registerTask('default', ['watch']);
-}
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        sourceMap: true,
+        style: 'compressed'
+      },
+      dist: {
+        files: {
+          'styles/css/main.css': 'styles/scss/main.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        use: [require('postcss-normalize')({})],
+        use: [require('postcss-flexbugs-fixes')({})],
+        processors: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions', 'ie 9']
+          }),
+          require('cssnano')(),
+        ]
+      },
+      dist: {
+        src: 'styles/css/*.css'
+      }
+    },
+
+    uglify: {
+      all_src: {
+        options: {
+          sourceMap: true,
+          sourceMapName: 'sourceMap.map'
+        },
+        src: 'scripts/**/*.js',
+        dest: 'scripts/main.min.js'
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['styles/scss/**/*.{scss,sass}'],
+        tasks: [
+          'sass', 'postcss'
+        ],
+        options: {
+          spawn: false,
+          livereload: false
+        },
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+
+};
---
+++ intro-to-web-design/glossary/index.md
@@ -0,0 +1,36 @@
+# Persona
+A summary of many people as one fictional person, from data gathered in research and discovery.
+
+# User
+The person who is using and interacting with  your product.
+
+# Stakeholder
+
+# User Story
+
+# Wireframe
+
+# Low-fidelity Wireframe
+
+# High-fidelity Wireframe
+
+# HTML
+Short for HyperText Markup Language - used to structure a web page.
+
+# CSS
+Short for Cascading Style Sheets - used to control how a page in HTML is displayed, affecting things like font styles, font sizes, colours, spacing, and positioning of elements.
+
+CMS
+Short for Content Management System -
+
+User Experience (UX)
+
+User Interface (UI)
+
+Information Architecture (IA)
+
+Javascript
+
+Prototype
+
+Product
